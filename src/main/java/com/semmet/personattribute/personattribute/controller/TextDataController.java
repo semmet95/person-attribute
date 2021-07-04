15b5034843f124efd117e502ff6f4dfe9370fffa
amit <singhamitch@gmail.com>
2021-07-05 00:27:06 +0530

    optimized mapping code

--- src/main/java/com/semmet/personattribute/personattribute/controller/TextDataController.java
+++ src/main/java/com/semmet/personattribute/personattribute/controller/TextDataController.java
@@ -1,19 +1,11 @@
 package com.semmet.personattribute.personattribute.controller;
 
-import java.util.List;
 import java.util.Map;
 
-import com.semmet.personattribute.personattribute.model.Entities;
-import com.semmet.personattribute.personattribute.model.KeyPhrases;
 import com.semmet.personattribute.personattribute.model.UserEntityMappings;
 import com.semmet.personattribute.personattribute.model.UserKeyPhraseMappings;
-import com.semmet.personattribute.personattribute.repository.EntitiesRepository;
-import com.semmet.personattribute.personattribute.repository.KeyPhrasesRepository;
-import com.semmet.personattribute.personattribute.repository.UserEntityMappingsRepository;
-import com.semmet.personattribute.personattribute.repository.UserKeyPhrasesMappingsRepository;
 import com.semmet.personattribute.personattribute.service.TextAnalysisService;
 import com.semmet.personattribute.personattribute.util.AWSComprehendUtil;
-import com.semmet.personattribute.personattribute.util.AppUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,14 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TextDataController class receives post requests on the path /text-data,
- * extracts insights on that text data using AWS Comprehend SDK,
- * then store the key phrases, entities, and their mappings with the
- * provided user
- * To check how the mappings are stored:
- * @see UserEntityMappings
- * @see UserKeyPhraseMappings
- * AWS Comprehend requests are sent using the utility class:
- * @see AWSComprehendUtil
+ * then forwards it to the TextAnalysisService class to analyze it using
+ * AWS Comprehend API and saves all the mappings between the user and the
+ * detected entities and key phrases
+ * @see TextAnalysisService
  * 
  * @author Amit Singh
  * @version 0.1
@@ -40,15 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/text-data")
 public class TextDataController {
-    
-    @Autowired
-    private EntitiesRepository entitiesRepository;
-    @Autowired
-    private KeyPhrasesRepository keyPhrasesRepository;
-    @Autowired
-    private UserEntityMappingsRepository userEntityMappingsRepository;
-    @Autowired
-    private UserKeyPhrasesMappingsRepository userKeyPhrasesMappingsRepository;
+
     @Autowired
     private TextAnalysisService textAnalysisService;
 
@@ -71,67 +51,6 @@ public class TextDataController {
         // keeping the string only alphanumeric and whitespaces
         textData = textData.replaceAll("[^a-zA-Z0-9\\s]", "");
 
-        // use Comprehend to extract all the data first
-        Map<String, Float> sentimentMap = textAnalysisService.analyzeText(textData, "en");
-        Entities[] allEntities = textAnalysisService.getEntitiesObjects();
-        KeyPhrases[] allKeyPhrases = textAnalysisService.getKeyPhrasesObjects();
-
-        // update the DB with new entities and key phrases
-        for(var entity: allEntities) {
-            if(entitiesRepository.findByEntity(entity.getEntity()).isEmpty()) {
-                entitiesRepository.save(entity);
-            }
-        }
-
-        for(var keyPhrase: allKeyPhrases) {
-            if(keyPhrasesRepository.findByKeyPhrase(keyPhrase.getKeyPhrase()).isEmpty()) {
-                keyPhrasesRepository.save(keyPhrase);
-            }
-        }
-
-        // update the DB with new mappings
-        UserEntityMappings[] allUserEntityMappings = textAnalysisService.getUserEntityMappingsObjects(userId);
-
-        for(var ueMapping: allUserEntityMappings) {
-            List<UserEntityMappings> existingueMappings = userEntityMappingsRepository.findByUser_UserIdAndEntity_Id(userId, ueMapping.getEntity().getId());
-
-            if(existingueMappings.isEmpty()) {
-                // handle the case with new ueMappings
-                userEntityMappingsRepository.save(ueMapping);
-            } else {
-                //update the existing ueMapping
-                var tempueMappings = existingueMappings.get(0);
-                tempueMappings.setFrequency(tempueMappings.getFrequency() + 1);
-                tempueMappings.setSentimentMixed(AppUtils.tanH(tempueMappings.getSentimentMixed(), sentimentMap.get("mixed")));
-                tempueMappings.setSentimentNegative(AppUtils.tanH(tempueMappings.getSentimentNegative(), sentimentMap.get("negative")));
-                tempueMappings.setSentimentNeutral(AppUtils.tanH(tempueMappings.getSentimentNeutral(), sentimentMap.get("neutral")));
-                tempueMappings.setSentimentPositive(AppUtils.tanH(tempueMappings.getSentimentPositive(), sentimentMap.get("positive")));
-                tempueMappings.setWeight(AppUtils.tanH(tempueMappings.getWeight(), ueMapping.getWeight()));
-
-                userEntityMappingsRepository.save(tempueMappings);
-            }
-        }
-
-        UserKeyPhraseMappings[] allUserKeyPhraseMappings = textAnalysisService.getUserKeyPhraseMappingsObjects(userId);
-
-        for(var ukpMapping: allUserKeyPhraseMappings) {
-            List<UserKeyPhraseMappings> existingukpMappings = userKeyPhrasesMappingsRepository.findByUser_UserIdAndKeyPhrase_Id(userId, ukpMapping.getKeyPhrase().getId());
-
-            if(existingukpMappings.isEmpty()) {
-                // handle the case with new ukpMappings
-                userKeyPhrasesMappingsRepository.save(ukpMapping);
-            } else {
-                //update the existing ukpMapping
-                var tempukpMappings = existingukpMappings.get(0);
-                tempukpMappings.setFrequency(tempukpMappings.getFrequency() + 1);
-                tempukpMappings.setSentimentMixed(AppUtils.tanH(tempukpMappings.getSentimentMixed(), sentimentMap.get("mixed")));
-                tempukpMappings.setSentimentNegative(AppUtils.tanH(tempukpMappings.getSentimentNegative(), sentimentMap.get("negative")));
-                tempukpMappings.setSentimentNeutral(AppUtils.tanH(tempukpMappings.getSentimentNeutral(), sentimentMap.get("neutral")));
-                tempukpMappings.setSentimentPositive(AppUtils.tanH(tempukpMappings.getSentimentPositive(), sentimentMap.get("positive")));
-                tempukpMappings.setWeight(AppUtils.tanH(tempukpMappings.getWeight(), ukpMapping.getWeight()));
-
-                userKeyPhrasesMappingsRepository.save(tempukpMappings);
-            }
-        }
+        textAnalysisService.addUserTextData(textData, "en", userId);
     }
 }
--- src/main/java/com/semmet/personattribute/personattribute/service/TextAnalysisService.java
+++ src/main/java/com/semmet/personattribute/personattribute/service/TextAnalysisService.java
@@ -1,5 +1,6 @@
 package com.semmet.personattribute.personattribute.service;
 
+import java.util.List;
 import java.util.Map;
 
 import com.semmet.personattribute.personattribute.exception.InvalidBodyExceptionHandler;
@@ -9,6 +10,8 @@ import com.semmet.personattribute.personattribute.model.UserEntityMappings;
 import com.semmet.personattribute.personattribute.model.UserKeyPhraseMappings;
 import com.semmet.personattribute.personattribute.repository.EntitiesRepository;
 import com.semmet.personattribute.personattribute.repository.KeyPhrasesRepository;
+import com.semmet.personattribute.personattribute.repository.UserEntityMappingsRepository;
+import com.semmet.personattribute.personattribute.repository.UserKeyPhrasesMappingsRepository;
 import com.semmet.personattribute.personattribute.repository.UserRepository;
 import com.semmet.personattribute.personattribute.util.AWSComprehendUtil;
 import com.semmet.personattribute.personattribute.util.AppLogger;
@@ -22,6 +25,13 @@ import org.springframework.stereotype.Service;
  * let you sends requests to, and get reponse from, AWS Comprehend
  * service. It also creates objects and their mappings in terms of
  * user objects and entity/key phrase objects.
+ * It extracts insights on text data then store the key phrases,
+ * entities, and their mappings with the provided user
+ * To check how the mappings are stored:
+ * @see UserEntityMappings
+ * @see UserKeyPhraseMappings
+ * AWS Comprehend requests are sent using the utility class:
+ * @see AWSComprehendUtil
  * 
  * @author Amit Singh
  * @version 0.1
@@ -37,195 +47,169 @@ public class TextAnalysisService {
     private KeyPhrasesRepository keyPhrasesRepository;
     @Autowired
     private UserRepository userRepository;
-
-    private Map<String, Float> sentimentMap;
-    private Map<String, Float> entityScoreMap;
-    private Map<String, Float> keyPhraseConfidenceMap;
-
-    public Map<String,Float> getSentimentMap() {
-        return this.sentimentMap;
-    }
-
-    public void setSentimentMap(Map<String,Float> sentimentMap) {
-        this.sentimentMap = sentimentMap;
-    }
-
-    public Map<String,Float> getEntityScoreMap() {
-        return this.entityScoreMap;
-    }
-
-    public void setEntityScoreMap(Map<String,Float> entityScoreMap) {
-        this.entityScoreMap = entityScoreMap;
-    }
-
-    public Map<String,Float> getKeyPhraseConfidenceMap() {
-        return this.keyPhraseConfidenceMap;
-    }
-
-    public void setKeyPhraseConfidenceMap(Map<String,Float> keyPhraseConfidenceMap) {
-        this.keyPhraseConfidenceMap = keyPhraseConfidenceMap;
-    }
+    @Autowired
+    private UserEntityMappingsRepository userEntityMappingsRepository;
+    @Autowired
+    private UserKeyPhrasesMappingsRepository userKeyPhrasesMappingsRepository;
 
     /**
-     * This method sends text data to Comprehend and saves the returned mappings
-     * of detected sentiments/key phrases/entities and their scores
-     * @see <a href="https://docs.aws.amazon.com/comprehend/latest/dg/how-entities.html">Comprehend Entities</a>
-     * @see <a href="https://docs.aws.amazon.com/comprehend/latest/dg/how-key-phrases.html">Comprehend Key Phrases</a>
-     * @see <a href="https://docs.aws.amazon.com/comprehend/latest/dg/how-sentiment.html">Comprehend Sentiments</a>
-     * 
-     * @param text the text data to be analyzed by Comprehend
-     * @param langCode ISO language code (currently hardcoded in every function call)
-     * @return mapping of the detected sentiments
-     */
-
-    public Map<String, Float> analyzeText(String text, String langCode) {
-        
-        this.setSentimentMap(AWSComprehendUtil.detectSentiment(text, langCode));
-        this.setEntityScoreMap(AWSComprehendUtil.detectAllEntities(text, langCode));
-        this.setKeyPhraseConfidenceMap(AWSComprehendUtil.detectAllKeyPhrases(text, langCode));
-
-        if(null == this.sentimentMap) {
-            AppLogger.LOGGER.error(String.format("received null sentiments for the text::%s", text));
-        } else if(null == this.entityScoreMap) {
-            AppLogger.LOGGER.error(String.format("received null entities for the text::%s", text));
-        } else if(null == this.keyPhraseConfidenceMap) {
-            AppLogger.LOGGER.error(String.format("received null key phrases for the text::%s", text));
-        }
-
-        return this.sentimentMap;
-    }
-
-    /**
-     * This method creates Entities objects from the entities mapping
-     * returned by Comprehend
+     * This method saves a new entity and returns its id or returns the id
+     * of the entity if its already saved
      * @see Entities
      * 
-     * @return array of the Entities objects
+     * @param entity entity to save/return id of
+     * @return id of the entity
      */
+    private long saveDetectedEntity(String entity) {
+        List<Entities> existingEntity = entitiesRepository.findByEntity(entity);
 
-    public Entities[] getEntitiesObjects() {
-        
-        Entities[] allEntities = new Entities[this.entityScoreMap.size()];
-        var index = 0;
-
-        for(var entities: this.entityScoreMap.keySet()) {
-
-            var tempEntities = new Entities();
-            tempEntities.setEntity(entities);
-
-            allEntities[index++] = tempEntities;
+        if(!existingEntity.isEmpty()) {
+            return existingEntity.get(0).getId();
         }
 
-        return allEntities;
+        var tempEntity = new Entities();
+        tempEntity.setEntity(entity);
+        tempEntity = entitiesRepository.save(tempEntity);
+
+        return tempEntity.getId();
     }
 
     /**
-     * This method creates KeyPhrases objects from the key phrases mapping
-     * returned by Comprehend
+     * This method saves a new key phrase and returns its id or returns the id
+     * of the key phrase if its already saved
      * @see KeyPhrases
      * 
-     * @return array of the KeyPhrases objects
+     * @param keyPhrase key phrase to save/return id of
+     * @return id of the key phrase
      */
+    private long saveDetectedKeyPhrase(String keyPhrase) {
+        List<KeyPhrases> existingEntity = keyPhrasesRepository.findByKeyPhrase(keyPhrase);
 
-    public KeyPhrases[] getKeyPhrasesObjects() {
-        
-        KeyPhrases[] allKeyPhrases = new KeyPhrases[this.keyPhraseConfidenceMap.size()];
-        var index = 0;
-
-        for(var keyPhrases: this.keyPhraseConfidenceMap.keySet()) {
-
-            var tempKeyPhrases = new KeyPhrases();
-            tempKeyPhrases.setKeyPhrase(keyPhrases);
-
-            allKeyPhrases[index++] = tempKeyPhrases;
+        if(!existingEntity.isEmpty()) {
+            return existingEntity.get(0).getId();
         }
 
-        return allKeyPhrases;
+        var tempEntity = new Entities();
+        tempEntity.setEntity(keyPhrase);
+        tempEntity = entitiesRepository.save(tempEntity);
+
+        return tempEntity.getId();
     }
 
     /**
-     * This method creates new user entity mapping objects between the
-     * user provided as an argument and the entities deteted by Comprehend
-     * @see UserEntityMappings
-     * 
-     * @param userId the user with which the analyzed text data was associated with
-     * @return array of the UserEntityMappings objects
-     */
-
-    public UserEntityMappings[] getUserEntityMappingsObjects(long userId) {
-        
-        UserEntityMappings[] allUserEntityMappings = new UserEntityMappings[entityScoreMap.size()];
-        var index = 0;
-
-        for(var entity: entityScoreMap.keySet()) {
-
-            var storedUser = userRepository.findByUserId(userId);
-            var tempueMappings = new UserEntityMappings();
-
-            if(storedUser.isEmpty()) {
-                AppLogger.LOGGER.error(String.format("user with userId:%d not found", userId));
-                throw new InvalidBodyExceptionHandler();
-            }
-            if(entitiesRepository.findByEntity(entity).isEmpty()) {
-                AppLogger.LOGGER.warn(String.format("entity:%s not found in the database, skipping mapping", entity));
-                continue;
-            }
-
-            tempueMappings.setEntity(entitiesRepository.findByEntity(entity).get(0));
-            tempueMappings.setUser(storedUser.get(0));
-            tempueMappings.setFrequency(1);
-            tempueMappings.setSentimentMixed(AppUtils.tanH(0, 2 * sentimentMap.get("mixed")));
-            tempueMappings.setSentimentNegative(AppUtils.tanH(0, 2 * sentimentMap.get("negative")));
-            tempueMappings.setSentimentNeutral(AppUtils.tanH(0, 2 * sentimentMap.get("neutral")));
-            tempueMappings.setSentimentPositive(AppUtils.tanH(0, 2 * sentimentMap.get("positive")));
-            tempueMappings.setWeight(AppUtils.tanH(0, 2 * entityScoreMap.get(entity)));
-
-            allUserEntityMappings[index++] = tempueMappings;
-        }
-
-        return allUserEntityMappings;
-    }
-
-    /**
-     * This method creates new user key phrase mapping objects between the
-     * user provided as an argument and the key phrases deteted by Comprehend
+     * This method creates a new mapping between a key phrase and a user
      * @see UserKeyPhraseMappings
      * 
-     * @param userId the user with which the analyzed text data was associated with
-     * @return array of the UserKeyPhraseMappings objects
+     * @param userId id of the user the key phrase is associated with
+     * @param keyPhraseId id of the key phrase that is to be associated with the user
+     * @param confidence key phrase score of the detected key phrase
+     * @param sentimentMap map of different detected sentiments and their corresponding scores
      */
+    private void saveUserKeyPhraseMapping(long userId, long keyPhraseId, float confidence, Map<String, Float> sentimentMap) {
+        var storedUser = userRepository.findByUserId(userId);
 
-    public UserKeyPhraseMappings[] getUserKeyPhraseMappingsObjects(long userId) {
-        
-        UserKeyPhraseMappings[] allUserKeyPhraseMappings = new UserKeyPhraseMappings[keyPhraseConfidenceMap.size()];
-        var index = 0;
-
-        for(var keyPhrase: keyPhraseConfidenceMap.keySet()) {
-
-            var storedUser = userRepository.findByUserId(userId);
-            var tempukpMappings = new UserKeyPhraseMappings();
-
-            if(storedUser.isEmpty()) {
-                AppLogger.LOGGER.error(String.format("user with userId:%d not found", userId));
-                throw new InvalidBodyExceptionHandler();
-            }
-            if(keyPhrasesRepository.findByKeyPhrase(keyPhrase).isEmpty()) {
-                AppLogger.LOGGER.warn(String.format("keyphrase:%s not found in the database, skipping mapping", keyPhrase));
-                continue;
-            }
-            
-            tempukpMappings.setFrequency(1);
-            tempukpMappings.setKeyPhrase(keyPhrasesRepository.findByKeyPhrase(keyPhrase).get(0));
-            tempukpMappings.setUser(storedUser.get(0));
-            tempukpMappings.setWeight(AppUtils.tanH(0, 2 * keyPhraseConfidenceMap.get(keyPhrase)));
-            tempukpMappings.setSentimentMixed(AppUtils.tanH(0, 2 * sentimentMap.get("mixed")));
-            tempukpMappings.setSentimentNegative(AppUtils.tanH(0, 2 * sentimentMap.get("negative")));
-            tempukpMappings.setSentimentNeutral(AppUtils.tanH(0, 2 * sentimentMap.get("neutral")));
-            tempukpMappings.setSentimentPositive(AppUtils.tanH(0, 2 * sentimentMap.get("positive")));
-
-            allUserKeyPhraseMappings[index++] = tempukpMappings;
+        if(storedUser.isEmpty()) {
+            AppLogger.LOGGER.error(String.format("user with userId:%d not found", userId));
+            throw new InvalidBodyExceptionHandler();
         }
+        if(keyPhrasesRepository.findById(keyPhraseId).isEmpty()) {
+            AppLogger.LOGGER.warn(String.format("key phrase id:%d not found in the database, skipping mapping", keyPhraseId));
+        }
+        else {
+            List<UserKeyPhraseMappings> existingukpMappings = userKeyPhrasesMappingsRepository.findByUser_UserIdAndKeyPhrase_Id(userId, keyPhraseId);
+            if(existingukpMappings.isEmpty()) {
 
-        return allUserKeyPhraseMappings;
+                var tempukpMappings = new UserKeyPhraseMappings();
+                tempukpMappings.setKeyPhrase(keyPhrasesRepository.findById(keyPhraseId).get());
+                tempukpMappings.setUser(storedUser.get(0));
+                tempukpMappings.setFrequency(1);
+                tempukpMappings.setSentimentMixed(AppUtils.tanH(0, 2 * sentimentMap.get("mixed")));
+                tempukpMappings.setSentimentNegative(AppUtils.tanH(0, 2 * sentimentMap.get("negative")));
+                tempukpMappings.setSentimentNeutral(AppUtils.tanH(0, 2 * sentimentMap.get("neutral")));
+                tempukpMappings.setSentimentPositive(AppUtils.tanH(0, 2 * sentimentMap.get("positive")));
+                tempukpMappings.setWeight(AppUtils.tanH(0, 2 * confidence));
+            } else {
+                //update the existing ueMapping
+                var tempukpMappings = existingukpMappings.get(0);
+                tempukpMappings.setFrequency(tempukpMappings.getFrequency() + 1);
+                tempukpMappings.setSentimentMixed(AppUtils.tanH(tempukpMappings.getSentimentMixed(), sentimentMap.get("mixed")));
+                tempukpMappings.setSentimentNegative(AppUtils.tanH(tempukpMappings.getSentimentNegative(), sentimentMap.get("negative")));
+                tempukpMappings.setSentimentNeutral(AppUtils.tanH(tempukpMappings.getSentimentNeutral(), sentimentMap.get("neutral")));
+                tempukpMappings.setSentimentPositive(AppUtils.tanH(tempukpMappings.getSentimentPositive(), sentimentMap.get("positive")));
+                tempukpMappings.setWeight(AppUtils.tanH(tempukpMappings.getWeight(), confidence));
+
+                userKeyPhrasesMappingsRepository.save(tempukpMappings);
+            }
+        }
+    }
+
+    /**
+     * This method creates a new mapping between an entity and a user
+     * @see UserEntityMappings
+     * 
+     * @param userId id of the user the entity is associated with
+     * @param entityId id of the entity that is to be associated with the user
+     * @param weight score weight of the detected entity
+     * @param sentimentMap map of different detected sentiments and their corresponding scores
+     */
+    private void saveUserEntityMapping(long userId, long entityId, float weight, Map<String, Float> sentimentMap) {
+        var storedUser = userRepository.findByUserId(userId);
+
+        if(storedUser.isEmpty()) {
+            AppLogger.LOGGER.error(String.format("user with userId:%d not found", userId));
+            throw new InvalidBodyExceptionHandler();
+        }
+        if(entitiesRepository.findById(entityId).isEmpty()) {
+            AppLogger.LOGGER.warn(String.format("entityId:%d not found in the database, skipping mapping", entityId));
+        }
+        else {
+            List<UserEntityMappings> existingueMappings = userEntityMappingsRepository.findByUser_UserIdAndEntity_Id(userId, entityId);
+            if(existingueMappings.isEmpty()) {
+
+                var tempueMappings = new UserEntityMappings();
+                tempueMappings.setEntity(entitiesRepository.findById(entityId).get());
+                tempueMappings.setUser(storedUser.get(0));
+                tempueMappings.setFrequency(1);
+                tempueMappings.setSentimentMixed(AppUtils.tanH(0, 2 * sentimentMap.get("mixed")));
+                tempueMappings.setSentimentNegative(AppUtils.tanH(0, 2 * sentimentMap.get("negative")));
+                tempueMappings.setSentimentNeutral(AppUtils.tanH(0, 2 * sentimentMap.get("neutral")));
+                tempueMappings.setSentimentPositive(AppUtils.tanH(0, 2 * sentimentMap.get("positive")));
+                tempueMappings.setWeight(AppUtils.tanH(0, 2 * weight));
+            } else {
+                //update the existing ueMapping
+                var tempueMappings = existingueMappings.get(0);
+                tempueMappings.setFrequency(tempueMappings.getFrequency() + 1);
+                tempueMappings.setSentimentMixed(AppUtils.tanH(tempueMappings.getSentimentMixed(), sentimentMap.get("mixed")));
+                tempueMappings.setSentimentNegative(AppUtils.tanH(tempueMappings.getSentimentNegative(), sentimentMap.get("negative")));
+                tempueMappings.setSentimentNeutral(AppUtils.tanH(tempueMappings.getSentimentNeutral(), sentimentMap.get("neutral")));
+                tempueMappings.setSentimentPositive(AppUtils.tanH(tempueMappings.getSentimentPositive(), sentimentMap.get("positive")));
+                tempueMappings.setWeight(AppUtils.tanH(tempueMappings.getWeight(), weight));
+
+                userEntityMappingsRepository.save(tempueMappings);
+            }
+        }
+    }
+
+    public void addUserTextData(String text, String langCode, long userId) {
+
+        Map<String, Float> sentimentMap = AWSComprehendUtil.detectSentiment(text, langCode);
+        Map<String, Float> entityScoreMap = AWSComprehendUtil.detectAllEntities(text, langCode);
+        Map<String, Float> keyPhraseConfidenceMap = AWSComprehendUtil.detectAllKeyPhrases(text, langCode);
+
+        entityScoreMap.entrySet().forEach(entry->{
+            var entity = entry.getKey();
+            var score = entry.getValue();
+
+            var entityId = saveDetectedEntity(entity);
+            saveUserEntityMapping(userId, entityId, score, sentimentMap);
+        });
+
+        keyPhraseConfidenceMap.entrySet().forEach(entry->{
+            var keyPhrase = entry.getKey();
+            var confidence = entry.getValue();
+
+            var keyPhraseId = saveDetectedKeyPhrase(keyPhrase);
+            saveUserKeyPhraseMapping(userId, keyPhraseId, confidence, sentimentMap);
+        });
     }
 }
